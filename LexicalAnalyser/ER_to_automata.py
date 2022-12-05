750e256d542968ea148f7d116a89be252f841019
Vitor Hugo <vitormarzarotto@hotmail.com>
2022-12-05 20:58:52 -0300

    fazendo

--- LexicalAnalyser/ER_to_automata.py
+++ LexicalAnalyser/ER_to_automata.py
@@ -27,24 +27,34 @@ class Node:
             if self.node_right:
                 self.node_right.show_leafs()
     
-    def get_leafs(self):
-        leafs = []
+    def get_leafs(self, leafs = []):
+      
         if self.node_left == None and self.node_right == None:
-          leafs.append(self)  
+            leafs.append(self)  
         else:
             if self.node_left:
-                self.node_left.show_leafs()
+                self.node_left.get_leafs(leafs)
             if self.node_right:
-                self.node_right.show_leafs()
-        return leafs
+                self.node_right.get_leafs(leafs)
+        return list(set(leafs))
     
 
     def tree_to_table(self):
         dic = dict()
         for leaf in self.get_leafs():
-            dic[str(leaf.id)] = list(leaf.followpos)
+            dic[leaf.type+str(leaf.id)] = [i.id for i in list(leaf.followpos)]
 
         return dic
+
+    def char_num(self):
+        dic =dict()
+        for i in tree.get_leafs():
+            if i.type in dic.keys():
+                dic[i.type].append(i.id)
+            else:
+                dic[i.type] = [i.id]
+        return dic
+    
 
     def __str__(self):
         if self.node_left == None and self.node_right == None:
@@ -152,14 +162,53 @@ class ER_to_automata:
         if regex[0] == ')' and regex[-1] == '(':
             return self.Er_to_tree(regex[1:-1])   
 
+def to_name(set_of):
 
+    list_nome = sorted([i.id for i in set_of])
+    nome = ''.join(list(map(str,list_nome)))
+    return nome
+
+def make_union(char,S,char_to):
+    ret = set()
+    for i in S:
+        for x in char_to[char] :
+            if i == x:
+                ret = ret.union(i)
+    return ret
+
+def is_in(U,automato,S):
+    u = to_name(U)
+    if u in automato.transition_table.keys():
+        return True
+    for i in S:
+        if u == to_name(i):
+            return True
+    return False
+
+
+def get_firtpos():
+
+def tree_to_afd(tree,inputs):
+    initial_state = to_name(tree.firstpos())
+    
+    automato = Automaton(initial_state,['a','b'])
+    
+    S = [tree.firstpos()]
+  
+    while len(S) > 0:
+        S.pop()
+        for i in automato.alfabet:
+            U = make_union(i,S,tree.char_num())
+            if not is_in(U,automato,S):
+               S.append(U)
+            automato.transition_table[to_name(U)] = (i,State(to_name(U)))
+    return automato
 
 
 
 
 if __name__ == '__main__':
     obj = ER_parser()
-
     obj.parseEr(os.path.join('ER','er_teste.txt'))
 
     automata_conv = ER_to_automata()
@@ -170,3 +219,9 @@ if __name__ == '__main__':
         tree.calculateFollowpos()
         print('-'*30)
         table = tree.tree_to_table()
+        tree.show_leafs()
+        
+        print(tree.char_num())
+        auto = tree_to_afd(tree,['a,b'])
+        print()
+        print(auto.transition_table)
