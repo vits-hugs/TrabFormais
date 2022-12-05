4e6b2f310aa33cfe87bf35b5a8b30eedd22ba7e6
Enzo <enzobassani09@gmail.com>
2022-12-04 22:03:12 -0300

    Update Automaton architecture

--- LexicalAnalyser/Automaton.py
+++ LexicalAnalyser/Automaton.py
@@ -1,11 +1,11 @@
 class Automaton:
     class State:
-        def __init__(self, name: str, transitions: dict(str, str), token_type: str):
+        def __init__(self, name: str, transitions: dict[str, str], token: tuple[str, str]):
             self.name = name
             self.transitions = transitions
-            self.token_type = token_type
+            self.token = token
 
-    def __init__(self, initial_state_name: str, transition_table: dict(str, State)):
+    def __init__(self, initial_state_name: str, transition_table: dict[str, State]):
         self.initial_state_name = initial_state_name
         self.transition_table = transition_table
 
