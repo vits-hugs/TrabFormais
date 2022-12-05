e0683c7022c87f666cea39674bb1333725378107
Pedro Henrique Dadalt de Queiroz <pedro.h.dt.qz@gmail.com>
2022-12-05 17:03:56 -0300

    Um monte de coisa:

--- AnalisadorLexico/SyntaticalAnalyzerGenerator.py
+++ AnalisadorLexico/SyntaticalAnalyzerGenerator.py
@@ -16,7 +16,67 @@ class RegularExpression:
     pass
 
 class ContextFreeGrammar:
-    pass
+    alphabet: set
+    initial_symbol: str
+    non_terminals: set
+    productions: dict
+    #ordered_productions is a list of tuples, where the first element of the tuple
+    #is the head of the production and the second is the body as a list of symbols.
+    ordered_productions: list
+
+    def __init__(self, alphabet: set, initial_symbol: str, non_terminals: str):
+        self.alphabet = alphabet
+        self.initial_symbol = initial_symbol
+        self.non_terminals = non_terminals
+
+    def __orderProductions__(self):
+        self.ordered_productions = []
+        for head, body in self.productions.items():
+            self.ordered_productions.push((head, body))
+
+    def __augment__(self):
+        initial_productions = self.productions[self.initial_symbol]
+        if (len(initial_productions) != 1 or len(initial_productions[0]) != 1):
+            initial_symbol = self.initial_symbol + "`"
+            self.productions[initial_symbol] = [[self.initial_symbol]]
+            self.initial_symbol = initial_symbol
+
+    def addProduction(self, head: str, body: list):
+        if (len(self.productions[head]) == 0):
+            self.productions[head] = []
+        self.productions[head].append(body)
+
+    def getInitialItem(self):
+        for i, production in enumerate(self.ordered_productions):
+            if (production[0] is self.initial_symbol):
+                return (i, 0)
+    
+    def first(self, symbol: str, non_terminals_visited = set()):
+        if (symbol in self.alphabet):
+            return set(symbol)
+        elif(symbol in self.non_terminals):
+            first_set = set()
+            non_terminals_visited.add(symbol)
+            for production in self.productions[symbol]:
+                if (production[0] is "&"):
+                    first_set.add("&")
+                else:
+                    for i, production_symbol in enumerate(production):
+                        if (production_symbol not in non_terminals_visited):
+                            production_symbol_first = self.first(production_symbol, non_terminals_visited)
+                            first_set = first_set.union(production_symbol_first)
+                            if ("&" not in production_symbol_first):
+                                break
+                        continue
+            return first_set
+        elif (symbol is "&"):
+            pass    
+        else:
+            return set()
+        
+
+    def follow(self):
+        pass
 
 class SyntaxTree:
     pass
@@ -25,20 +85,24 @@ class SymbolTable:
     pass
 
 class FiniteStateAutomaton:
-    alphabet: set(str)
-    states: set(str)
-    accepting_states: set(str)
+    alphabet: set()
+    states: set()
+    accepting_states: set()
     initial_state: str
-    transition_table: dict((str,str))
+    transition_table: dict()
 
-    def __init__(self, alphabet):
+    def __init__(self, alphabet = set(), states = set(), accepting_states = set(), initial_state = set(), transition_table = dict()):
         self.alphabet = alphabet
+        self.states = states
+        self.accepting_states = accepting_states
+        self.initial_state = initial_state
+        self.transition_table = transition_table
 
 class DeterministicAutomaton(FiniteStateAutomaton):
-    transition_table: dict((str,str),str)
+    transition_table: dict
 
 class NonDeterministicAutomaton(FiniteStateAutomaton):
-    transition_table: dict((str,str),set(str))
+    transition_table: dict
 
 class LexicalPatternMatcher:
     lexemme_automaton: DeterministicAutomaton
@@ -47,11 +111,12 @@ class LexicalPatternMatcher:
     looking_ahead: bool
     lookahed_count: int
     idle: bool
-    getToken: dict(str,function)
+    getToken: dict
 
-    def __init__(self, lexemme_automaton: DeterministicAutomaton, lookahead_automaton: DeterministicAutomaton):
+    def __init__(self, lexemme_automaton: DeterministicAutomaton, lookahead_automaton: DeterministicAutomaton, getToken: dict):
         self.lexemme_automaton = lexemme_automaton
         self.lookahead_automaton = lookahead_automaton
+        self.getToken = getToken
         self.__reset__()
 
     def __reset__(self):
@@ -104,15 +169,15 @@ class LexicalPatternMatcher:
 
 class LexicalAnalyzer:
     source_reader: StringIO
-    white_spaces: list[chr]
+    white_spaces: list
     pattern_matcher: LexicalPatternMatcher
-    symbol_table: SymbolTable
+    #symbol_table: SymbolTable
 
-    def __init__(self, pattern_matcher: LexicalPatternMatcher, white_spaces = [' ', '\t', '\n'], symbol_table: SymbolTable):
+    def __init__(self, pattern_matcher: LexicalPatternMatcher, white_spaces = [' ', '\t', '\n']):
         self.source_reader = StringIO()
         self.white_spaces = white_spaces
         self.pattern_matcher = pattern_matcher
-        self.symbol_table = symbol_table
+        #self.symbol_table = symbol_table
 
     def loadSource(self, source):
         self.source_reader.open(source, "r", encoding="utf-8")
@@ -120,7 +185,7 @@ class LexicalAnalyzer:
     def getNextToken(self):
         lexem_buffer = ""
         while (True):
-            c = self.source_reader.read()
+            c = self.source_reader.read(1)
             if (c is None):
                 return None
             elif (self.pattern_matcher.idle):
@@ -139,18 +204,94 @@ class LexicalAnalyzer:
                 self.source_reader.seek(self.source_reader.tell() - matching_result[1])
                 return matching_result[0]
 
-class SyntacticalAnalyzer:
-    lexical_analyser: LexicalAnalyzer
+class LRParser:
+    #symbol_table: SymbolTable
+    #lexical_analyser: LexicalAnalyzer
+    grammar: ContextFreeGrammar
+    canonical_collection: list
+    stack: list
+    action: dict
+    goto: dict
+    srl_parsing_table: dict
+
+    def __init__(self, grammar: ContextFreeGrammar):
+        self.grammar = grammar
+        self.stack = ["$"]
+        self.action = {}
+        self.goto = {}
+        self.srl_parsing_table = {}
+        self.grammar.__augment__()
+        self.grammar.__orderProductions__()
+        self.canonical_collection = self.__getCanonicalCollection__()
+
+
+    def __closure__(self, set_of_items: set):
+        closure_set = set_of_items.copy()
+        new_items = set()
+        while (True):
+            prev_items = new_items.copy()
+            for item in closure_set:
+                if (item[1] == len(self.grammar.ordered_productions[item[0]][1])):
+                    #The above comparison means that the dot is after the whole body of the production
+                    continue
+                next_symbol = self.grammar.ordered_productions[item[0]][1][item[1]]
+                if (next_symbol in self.grammar.non_terminals):
+                    for i, production_tuple in enumerate(self.grammar.ordered_productions):
+                        if (production_tuple[0] == next_symbol):
+                            new_items.add((i,0))
+            if (prev_items == new_items):
+                return closure_set
+            else:
+                closure_set.add(new_items)
+                prev_items = new_items.copy()
+
+    def __goto__(self, set_of_items: set, symbol: str):
+        goto_set = set()
+        for item in set_of_items:
+            if (item[1] == len(self.grammar.ordered_productions[item[0]][1])):
+                #The above comparison means that the dot is after the whole body of the production
+                continue
+            if (self.grammar.ordered_productions[item[0]][1][item[1]] is symbol):
+                goto_set.add((item[0], item[1] + 1))
+        return self.__closure__(goto_set)
+            
+    def __getCanonicalCollection__(self):
+        cc = set()
+        cc.add(self.__closure__(self.grammar.getInitialItem()))
+        while (True):
+            cc_len = len(cc)
+            for set_of_items in cc:
+                for symbol in self.grammar.alphabet:
+                    goto_set = self.__goto__(set_of_items, symbol)
+                    if (len(goto_set) > 0):
+                        cc.add(goto_set)
+            if (len(cc) == cc_len):
+                return list(cc)
+    
+    def __constructSLRParsingTable__(self):
+        for i, canonicalSet in enumerate(self.canonical_collection):
+            for item in canonicalSet:
+                if (item[1] == len(self.grammar.ordered_productions[item[0]][1])):
+                    
+                symbol = self.grammar.ordered_productions[item[0]][1][item[1]]
+                if (symbol in self.grammar.alphabet):
+                    j_set = self.__goto__(canonicalSet, symbol)
+                    if (j_set in self.canonical_collection):
+                        self.action[i, symbol] = ("s", self.canonical_collection.index(j_set))
+                
+
+
+
 
 class LexicalAnalyzerGenerator:
     lexemme_automaton: DeterministicAutomaton
     lookahead_automaton: DeterministicAutomaton
-    re_parser: SyntacticalAnalyzer
+    re_parser: LRParser
 
     def __init__(self):
         pass
 
-    def __createAutomatons__(self, alphabet: set(str)):
+    def __createAutomatons__(self, alphabet: set()):
         self.lexemme_automaton = DeterministicAutomaton(alphabet)
         self.lookahead_automaton = DeterministicAutomaton(alphabet)
 
@@ -161,3 +302,16 @@ class SyntacticalAnalyzerGenerator:
     
     class GrammarDefinitionReader:
         pass
+
+def getTokenFun():
+    print("ok")
+
+if (__name__ == "__main__"):
+    transition_table = {("i","a"): "i", ("i","b"): "1", ("1","a"): "f"}
+    getToken = {"f": getTokenFun}
+    lexemme_automaton = DeterministicAutomaton(alphabet = {"a","b"}, states = {"i", "1", "f"}, \
+                                                accepting_states={"f"}, initial_state={"i"}, \
+                                                transition_table=transition_table)
+    pattern_matcher = LexicalPatternMatcher(lexemme_automaton=lexemme_automaton, lookahead_automaton=DeterministicAutomaton(), getToken=getToken)
+    lexical_analyzer = LexicalAnalyzer(pattern_matcher=pattern_matcher)
+    lexical_analyzer.loadSource("./in.txt")
