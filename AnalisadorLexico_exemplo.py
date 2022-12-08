34270886b7d34730343f5aeea505e1d7d36e80aa
Enzo <enzobassani09@gmail.com>
2022-12-08 00:53:37 -0300

    Update example

--- AnalisadorLexico_exemplo.py
+++ AnalisadorLexico_exemplo.py
@@ -9,9 +9,11 @@ import os
 file_id = 2
 name_files = ['detecto_texto','exemplo_aula','exemplo_trabalho']
 
-teste_string = ['A sou o Vitor tenho 19 anos, 173cm , nasci em 2022 , e gosto de escrever abaababbbbab em folhas A4',
+teste_string = [
+                'A sou o Vitor tenho 19 anos, 173cm , nasci em 2022 , e gosto de escrever abaababbbbab em folhas A4',
                 'abababbababba',
-                'coisa2 a 2 aa23']
+                'Sonim123 e blainim e 80'
+                ]
 
 ERs_file = open(os.path.join('ER', name_files[file_id]+'.txt'))
 REs = ERs_file.read()
