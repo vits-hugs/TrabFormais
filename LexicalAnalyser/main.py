92c4140838fe82dac69c56cc38185e6f088eaa2c
Enzo <enzobassani09@gmail.com>
2022-12-07 21:41:29 -0300

    Add example script

--- LexicalAnalyser/main.py
+++ LexicalAnalyser/main.py
@@ -1,24 +1,18 @@
 from LexicalAnalyserGenerator import LexicalAnalyserGenerator
 import os
 
-files = ['er_1','er_pteste','er_teste','parser_texto']
-lista = ['a 3 gatos comeram minha folha A4',
-        ' a  a aa aaaa aaaaaaaaaaaa',
-        'abababc cd ablm alm c d  ',
-        'eu sou Enzo tenho 10cm e 180cm de altura , nasci em  2022 , sou amigo do Vitor e sim A4 e uma palavra abbababababababababbabaabb']
-for name_file in files:
-    file = open(os.path.join('ER', name_file + '.txt'))
-    string = file.read()
-    file.close()
-    print(name_file)
-    LA = LexicalAnalyserGenerator.getLexicalAnalyser(string)
+ERs_file = open(os.path.join('ER', 'er_1.txt'))
+REs = ERs_file.read()
+print("Expressões regulares: \n", REs, sep='')
+ERs_file.close()
 
-    tokens, symbolList = LA.exec(lista.pop(0))
-    print("SymbolList")
-    symbolList.print()
-    print()
-    print('-'*30)
-    print()
+LA = LexicalAnalyserGenerator.getLexicalAnalyser(REs)
 
-#print("Tokens: ", tokens)
+tokens, symbolList = LA.exec("Sonim Blainim123 425 s sinim")
 
+print("Tokens: ")
+for token in tokens:
+    print(token)
+print()
+print("SymbolList")
+symbolList.print()
