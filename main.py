2a64bc05991aa3f2182f4750cebb881b392643e8
Vitor Hugo <vitormarzarotto@hotmail.com>
2022-12-07 23:55:34 -0300

    testes

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from LexicalAnalyser.LexicalAnalyserGenerator import LexicalAnalyserGenerator
 import os
 
-ERs_file = open(os.path.join('ER', 'detecto_texto.txt'))
+file_id = 2
+name_files = ['detecto_texto','exemplo_aula','exemplo_trabalho']
+
+teste_string = ['A sou o Vitor tenho 19 anos, 173cm , nasci em 2022 , e gosto de escrever abaababbbbab em folhas A4',
+                'abababbababba',
+                'coisa2 a 2 aa23']
+
+ERs_file = open(os.path.join('ER', name_files[file_id]+'.txt'))
 REs = ERs_file.read()
 print("Expressões regulares: \n", REs, sep='')
 ERs_file.close()
 
 LA = LexicalAnalyserGenerator.getLexicalAnalyser(REs)
 
-tokens, symbolList = LA.exec("Sonim Blainim123 425 s sinim")
+tokens, symbolList = LA.exec(teste_string[file_id])
 
 print("Tokens: ")
 for token in tokens:
